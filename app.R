59eb4f02b94b7fee66f78e122ee9a20a759d5b45
thedivtagguy <amanbhargava2001@gmail.com>
2024-01-01 13:37:02 +0530

    Draft app

--- app.R
+++ app.R
@@ -1,48 +1,73 @@
 library(shiny)
 library(tidyverse)
+library(shinydashboard)
+library(DT)  
 source('connections.r')
 source('wordle.r')
 
-ui <- fluidPage(
-  titlePanel("Game Results Logger"),
-  sidebarLayout(
-    sidebarPanel(
-      textAreaInput("a_connections", "Player A Connections", ""),
-      textAreaInput("b_connections", "Player B Connections", ""),
-      textAreaInput("a_wordle", "Player A Wordle", ""),
-      textAreaInput("b_wordle", "Player B Wordle", ""),
-      actionButton("submit_connections", "Submit Connections"),
-      actionButton("submit_wordle", "Submit Wordle")
+ui <- dashboardPage(
+  dashboardHeader(title = "Game Results Logger"),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    fluidRow(
+      box(title = "Game Inputs", status = "primary", solidHeader = TRUE, width = 12, collapsible = TRUE,
+          fluidRow(
+            column(4,
+                   textAreaInput("a_connections", "A Connections", ""),
+                   textAreaInput("b_connections", "R Connections", ""),
+                   
+            ),
+            
+            column(4,
+                   textAreaInput("a_wordle", "A Wordle", ""),
+                   textAreaInput("b_wordle", "R Wordle", "")
+            )
+          ),
+          fluidRow(
+            column(12,
+                   actionButton("submit", "Submit", class = "btn-primary")
+            )
+          )
+      )
     ),
-    mainPanel(
-      tableOutput("resultsTable")
+    fluidRow(
+      box(title = "Results", status = "info", solidHeader = TRUE, width = 12,
+          DT::dataTableOutput("resultsTable")
+      )
     )
   )
 )
 
+
 server <- function(input, output, session) {
   results <- reactiveVal(tibble(date = as.Date(character()), gameType = character(), wonBy = character(), margin = integer()))
   
-  observeEvent(input$submit_connections, {
-    a_processed <- process_connections(input$a_connections, "Player A")
-    b_processed <- process_connections(input$b_connections, "Player B")
-    judgement <- judge_connections(a_processed, b_processed)
-    
-    new_entry <- tibble(date = Sys.Date(), gameType = "Connections", wonBy = judgement$wonBy, margin = judgement$margin)
-    results(bind_rows(results(), new_entry))
+  observeEvent(input$submit, {
+    if (isTruthy(input$a_connections) && isTruthy(input$b_connections) &&
+        isTruthy(input$a_wordle) && isTruthy(input$b_wordle)) {
+      
+      a_con_processed <- process_connections(input$a_connections, "Player A")
+      b_con_processed <- process_connections(input$b_connections, "Player R")
+      con_judgement <- judge_connections(a_con_processed, b_con_processed)
+      
+      wordle_a_processed <- process_wordle(input$a_wordle, "Player A")
+      wordle_b_processed <- process_wordle(input$b_wordle, "Player R")
+      wordle_judgement <- judge_wordle(wordle_a_processed, wordle_b_processed)
+      
+      new_entries <- tribble(
+        ~date, ~gameType, ~wonBy, ~margin,
+        Sys.Date(), "Connections", con_judgement$wonBy, con_judgement$margin,
+        Sys.Date(), "Wordle", wordle_judgement$wonBy, wordle_judgement$margin
+      )
+      
+      results(bind_rows(results(), new_entries))
+    } else {
+      shiny::showNotification("Please fill in all fields before submitting.", type = "error")
+    }
   })
   
-  observeEvent(input$submit_wordle, {
-    a_processed <- process_wordle(input$a_wordle, "Player A")
-    b_processed <- process_wordle(input$b_wordle, "Player B")
-    judgement <- judge_wordle(a_processed, b_processed)
-    
-    new_entry <- tibble(date = Sys.Date(), gameType = "Wordle", wonBy = judgement$wonBy, margin = judgement$margin)
-    results(bind_rows(results(), new_entry))
-  })
-  
-  output$resultsTable <- renderTable({
-    results()
+  output$resultsTable <- DT::renderDataTable({
+    DT::datatable(results(), options = list(pageLength = 5, autoWidth = TRUE))
   })
 }
 
